c2e65eec3efc8c76ea0b8c59f1d4c092838e3491
Ricky <Ricky@sasvm>
2014-04-08 15:48:08 -0400

    Bump up image size 10 times.

--- runtime/graphdriver/devmapper/deviceset.go
+++ runtime/graphdriver/devmapper/deviceset.go
@@ -20,9 +20,9 @@ import (
 )
 
 var (
-	DefaultDataLoopbackSize     int64  = 100 * 1024 * 1024 * 1024
-	DefaultMetaDataLoopbackSize int64  = 2 * 1024 * 1024 * 1024
-	DefaultBaseFsSize           uint64 = 10 * 1024 * 1024 * 1024
+	DefaultDataLoopbackSize     int64  = 1000 * 1024 * 1024 * 1024
+	DefaultMetaDataLoopbackSize int64  = 20 * 1024 * 1024 * 1024
+	DefaultBaseFsSize           uint64 = 100 * 1024 * 1024 * 1024
 )
 
 type DevInfo struct {
