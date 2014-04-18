80797ef45123f5ea2b4a3a76cf13b132d955832c
Ricky Zhang <ricky.zhang@sas.com>
2014-04-18 11:30:36 -0400

    Shrink down metadata size

--- daemon/graphdriver/devmapper/deviceset.go
+++ daemon/graphdriver/devmapper/deviceset.go
@@ -26,7 +26,7 @@ import (
 
 var (
 	DefaultDataLoopbackSize     int64  = 1000 * 1024 * 1024 * 1024
-	DefaultMetaDataLoopbackSize int64  = 20 * 1024 * 1024 * 1024
+	DefaultMetaDataLoopbackSize int64  = 15 * 1024 * 1024 * 1024
 	DefaultBaseFsSize           uint64 = 100 * 1024 * 1024 * 1024
 )
 
